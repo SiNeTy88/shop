67c611eef60081408d2ef14939d40ad2ef5bfc45
nretrorsum <87096143+nretrorsum@users.noreply.github.com>
2026-01-10 00:34:05 +0200

    Update urls.py

--- shop/urls.py
+++ shop/urls.py
@@ -1,5 +1,6 @@
 # from .views import ProductViewSet
 
+#Чому закоментовано, не буде ж працювати
 # from rest_framework.routers import DefaultRouter
 # router = DefaultRouter()
 # router.register(r'products', ProductViewSet, basename='Products')
